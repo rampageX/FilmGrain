8b81dd00f1438ff8b1538944b9f346459971318b
rampageX <punkdm@gmail.com>
2026-09-18 14:32:43 +0800

    test: bump i18n bugfix suffix to R4N8C

--- Utils/FilmGrain_Studio.ps1
+++ Utils/FilmGrain_Studio.ps1
@@ -783,7 +783,7 @@ $statusVersion = New-Object System.Windows.Forms.ToolStripStatusLabel
 $statusVersion.Spring = $false
 $statusVersion.TextAlign = [System.Drawing.ContentAlignment]::MiddleRight
 $statusVersion.ForeColor = $ColorMuted
-$statusVersion.Text = 'v4.7.5 I18N TEST K7M2Q'
+$statusVersion.Text = 'v4.7.5 I18N TEST R4N8C'
 $statusVersion.Margin = New-Object System.Windows.Forms.Padding -ArgumentList 12, 0, 0, 0
 [void]$statusStrip.Items.Add($statusVersion)
 
